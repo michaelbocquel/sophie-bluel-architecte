18f11bee404a586b3d383da8885034bc5d343881
noscatis <michael.bocquel@gmail.com>
2024-10-04 18:05:00 +0200

    added modal to add a project

--- FrontEnd/scripts/modal.js
+++ FrontEnd/scripts/modal.js
@@ -1,27 +1,42 @@
 import { getData } from "./get-data.js";
 
 const modal = document.querySelector(".modal");
+const modalPhotoGallery = document.querySelector(".modal__photo__gallery");
+const modalPhotoAdd = document.querySelector(".modal__photo__add");
 const portfolioModifyButton = document.querySelector(
 	".portfolio__modify__button"
+);
+const modalPhotoGalleryAddButton = document.querySelector(
+	".modal__photo__gallery__add__button"
 );
 
 portfolioModifyButton.addEventListener("click", () => {
 	modal.showModal();
 });
 
+modalPhotoGalleryAddButton.addEventListener("click", () => {
+	modalPhotoGallery.classList.add("hidden");
+	modalPhotoAdd.classList.remove("hidden");
+});
+
 export const displayModalGallery = () => {
 	getData().then((data) => {
 		data.forEach((element) => {
-			console.log(element);
 			const image = document.createElement("img");
 
 			image.classList.add("modal__image");
 
 			image.src = element.imageUrl;
 
-			modal.append(image)
+			modalPhotoGallery.append(image);
 		});
 	});
 };
 
 displayModalGallery();
+
+modal.addEventListener("click", (event) => {
+	if (event.target === modal) {
+		modal.close();
+	}
+});
