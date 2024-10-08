073167dbc98835300b1f1df3d06e1f53d649a551
noscatis <michael.bocquel@gmail.com>
2024-10-08 14:54:38 +0200

    added interaction between the two parts of the modal

--- FrontEnd/scripts/modal.js
+++ FrontEnd/scripts/modal.js
@@ -2,6 +2,9 @@ import { getData } from "./get-data.js";
 
 const modal = document.querySelector(".modal");
 const modalPhotoGallery = document.querySelector(".modal__photo__gallery");
+const modalPhotoGalleryTitle = document.querySelector(
+	".modal__photo__gallery__title"
+);
 const modalPhotoAdd = document.querySelector(".modal__photo__add");
 const portfolioModifyButton = document.querySelector(
 	".portfolio__modify__button"
@@ -9,6 +12,7 @@ const portfolioModifyButton = document.querySelector(
 const modalPhotoGalleryAddButton = document.querySelector(
 	".modal__photo__gallery__add__button"
 );
+const backArrow = document.querySelector(".fa-arrow-left");
 
 portfolioModifyButton.addEventListener("click", () => {
 	modal.showModal();
@@ -19,12 +23,16 @@ modalPhotoGalleryAddButton.addEventListener("click", () => {
 	modalPhotoAdd.classList.remove("hidden");
 });
 
+backArrow.addEventListener("click", () => {
+	modalPhotoGallery.classList.remove("hidden");
+	modalPhotoAdd.classList.add("hidden");
+});
+
 const token = localStorage.getItem("token");
 
 export const displayModalGallery = () => {
 	getData().then((data) => {
-		for (let i = 1; i <= data.length; i++) {
-			console.log(i);
+		for (let i = 0; i <= data.length; i++) {
 			const image = document.createElement("img");
 			const iconDiv = document.createElement("div");
 			const icon = document.createElement("i");
@@ -36,19 +44,41 @@ export const displayModalGallery = () => {
 
 			image.src = data[i].imageUrl;
 
+			modalPhotoGalleryAddButton.addEventListener("click", () => {
+				image.classList.add("hidden");
+				iconDiv.classList.add("hidden");
+				modalPhotoGalleryTitle.classList.add("hidden");
+				modalPhotoGalleryAddButton.classList.add("hidden");
+			});
+
+			backArrow.addEventListener("click", () => {
+				image.classList.remove("hidden");
+				iconDiv.classList.remove("hidden");
+				modalPhotoGalleryTitle.classList.remove("hidden");
+				modalPhotoGalleryAddButton.classList.remove("hidden");
+			});
+
 			modalPhotoGallery.append(image);
 			modalPhotoGallery.append(iconDiv);
 			iconDiv.append(icon);
 
 			iconDiv.addEventListener("click", () => {
-				fetch("http://localhost:5678/api/works/" + i, {
-					method: "DELETE",
-					headers: {
-						Accept: "application/json",
-						"Content-Type": "application/json",
-						Authorization: `Bearer ${token}`,
-					},
-				}).then((res) => console.log(res));
+				const response = fetch(
+					"http://localhost:5678/api/works/" + `${data[i].id}`,
+					{
+						method: "DELETE",
+						headers: {
+							Accept: "application/json",
+							"Content-Type": "application/json",
+							Authorization: `Bearer ${token}`,
+						},
+					}
+				).then((res) => {
+					if (res.status === 204) {
+						image.remove();
+						iconDiv.remove();
+					}
+				});
 			});
 		}
 	});
