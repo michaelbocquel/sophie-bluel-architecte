a716449f95dabdceb4300e1492212dd0c2d4a01e
noscatis <michael.bocquel@gmail.com>
2024-10-14 20:46:33 +0200

    used get categories script to create my filters

--- FrontEnd/scripts/filter-gallery.js
+++ FrontEnd/scripts/filter-gallery.js
@@ -1,36 +1,25 @@
 import { getData } from "./get-data.js";
+import { getCategories } from "./get-categories.js";
+
+const filterButtons = document.querySelectorAll(".filter__button");
 
 export const filterGallery = () => {
-	getData().then((data) => {
-		const categories = [];
-		data.forEach((element) => {
-			categories.push(element.category.name);
-		});
-		let uniqueCategories = Array.from(new Set(categories));
+	getCategories().then((data) => {
+		for (let i = 0; i < data.length; i++) {
+			filterButtons[i].innerHTML = data[i].name;
+		}
+	});
 
+	getData().then((data) => {
+		const resetButton = document.querySelector(".reset__button");
+		resetButton.addEventListener("click", () => {
+			figures.forEach((figure) => {
+				figure.classList.remove("hidden");
+			});
+		});
 		const gallery = document.querySelector(".gallery");
 		const figures = document.querySelectorAll(".gallery__figure");
-
-		const portfolioTitle = document.querySelector(".portfolio__title");
-		const filterButtonMenu = document.createElement("div");
-		filterButtonMenu.classList.add("filter__button__menu");
-		const resetButton = document.createElement("button");
-		resetButton.classList.add("reset__button");
-		portfolioTitle.append(filterButtonMenu);
-		filterButtonMenu.appendChild(resetButton);
-		resetButton.innerHTML = "Tous";
-
-		const token = localStorage.getItem("token");
-
-		if (token) {
-			filterButtonMenu.style.display = "none";
-		}
-
-		uniqueCategories.forEach((element) => {
-			const filterButton = document.createElement("button");
-			filterButton.classList.add("filter__button");
-			filterButtonMenu.appendChild(filterButton);
-			filterButton.innerHTML = element;
+		filterButtons.forEach((filterButton) => {
 			filterButton.addEventListener("click", () => {
 				for (let n = 0; n < data.length; n++) {
 					if (data[n].category.name !== filterButton.textContent) {
@@ -41,13 +30,6 @@ export const filterGallery = () => {
 				}
 			});
 		});
-
-		resetButton.addEventListener("click", () => {
-			figures.forEach((figure) => {
-				figure.classList.remove("hidden");
-			});
-		});
 	});
 };
-
 filterGallery();
