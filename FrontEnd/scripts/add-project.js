db0511d67c5f751d34e420f3c028e65198069214
noscatis <michael.bocquel@gmail.com>
2024-10-14 20:47:40 +0200

    finalized modal scripts

--- FrontEnd/scripts/add-project.js
+++ FrontEnd/scripts/add-project.js
@@ -1,27 +1,50 @@
-import { getData } from "./get-data.js";
+import { getCategories } from "./get-categories.js";
 
-const modalFormSubmit = document.querySelector(".modal__form__submit");
+const modalForm = document.querySelector(".modal__form");
+const modalFormPhoto = document.querySelector(".modal__form__photo");
+const modalFormButton = document.querySelector(".modal__form__button");
+const modalFormSizeInfo = document.querySelector(".modal__form__size__info");
+const modalTitleInput = document.querySelector(".modal__title__input");
+const modalCategorySelect = document.querySelector(".modal__category__select");
+const modalImageInput = document.querySelector(".modal__image__input");
 
 const token = localStorage.getItem("token");
 
-export const addProject = () => {
-	getData().then((data) => {
-		modalFormSubmit.addEventListener("click", () => {
-			fetch("http://localhost:5678/api/works", {
-				method: "POST",
-				headers: {
-					Accept: "application/json",
-					"Content-Type": "application/json",
-					Authorization: `Bearer ${token}`,
-				},
-				body: JSON.stringify({
-					image: image.value,
-					title: title.value,
-					category: category.value,
-				}),
-			}).then((res) => {
-				console.log(res)
-			});
-		});
+modalImageInput.addEventListener("change", (event) => {
+	modalFormPhoto.src = window.URL.createObjectURL(event.target.files[0]);
+	modalFormButton.classList.add('hidden')
+	modalFormSizeInfo.classList.add('hidden')
+});
+
+getCategories().then((data) => {
+	for (let i = 0; i < data.length; i++) {
+		const modalCategoryOption = document.createElement("option");
+		modalCategoryOption.value = i + 1;
+		modalCategoryOption.innerHTML = data[i].name;
+		modalCategorySelect.append(modalCategoryOption);
+	}
+});
+
+export const addProject = (e) => {
+	e.preventDefault();
+	let formData = new FormData();
+	formData.append("title", modalTitleInput.value);
+	formData.append("category", parseInt(modalCategorySelect.value));
+	formData.append("image", modalImageInput.files[0]);
+	fetch("http://localhost:5678/api/works", {
+		method: "POST",
+		headers: {
+			Accept: "application/json",
+			Authorization: `Bearer ${token}`,
+		},
+		body: formData,
+	}).then((response) => {
+		if (response.status === 201) {
+			window.location.reload();
+		}
 	});
+};
+modalForm.onsubmit = (e) => {
+	e.preventDefault();
+	addProject(e);
 };
--- FrontEnd/scripts/modal.js
+++ FrontEnd/scripts/modal.js
@@ -1,38 +1,44 @@
 import { getData } from "./get-data.js";
 
 const modal = document.querySelector(".modal");
+const modalPhotoGalleryDiv = document.querySelector(
+	".modal__photo__gallery__div"
+);
 const modalPhotoGallery = document.querySelector(".modal__photo__gallery");
+const modalPhotoGallerySeparator = document.querySelector(
+	".modal__photo__gallery__separator"
+);
 const modalPhotoGalleryTitle = document.querySelector(
 	".modal__photo__gallery__title"
 );
 const modalPhotoAdd = document.querySelector(".modal__photo__add");
-const portfolioModifyButton = document.querySelector(
-	".portfolio__modify__button"
-);
+const portfolioModifyDiv = document.querySelector(".portfolio__modify__div");
 const modalPhotoGalleryAddButton = document.querySelector(
 	".modal__photo__gallery__add__button"
 );
 const backArrow = document.querySelector(".fa-arrow-left");
 
-portfolioModifyButton.addEventListener("click", () => {
-	modal.showModal();
-});
-
 modalPhotoGalleryAddButton.addEventListener("click", () => {
-	modalPhotoGallery.classList.add("hidden");
+	modalPhotoGallery.style.display = "none";
 	modalPhotoAdd.classList.remove("hidden");
 });
 
 backArrow.addEventListener("click", () => {
-	modalPhotoGallery.classList.remove("hidden");
+	modalPhotoGallery.style.display = "flex";
 	modalPhotoAdd.classList.add("hidden");
 });
 
 const token = localStorage.getItem("token");
 
+if (token) {
+	portfolioModifyDiv.addEventListener("click", () => {
+		modal.showModal();
+	});
+}
+
 export const displayModalGallery = () => {
 	getData().then((data) => {
-		for (let i = 0; i <= data.length; i++) {
+		for (let i = 0; i < data.length; i++) {
 			const image = document.createElement("img");
 			const iconDiv = document.createElement("div");
 			const icon = document.createElement("i");
@@ -45,16 +51,21 @@ export const displayModalGallery = () => {
 			image.src = data[i].imageUrl;
 
 			modalPhotoGalleryAddButton.addEventListener("click", () => {
+				modalPhotoGalleryDiv;
 				image.classList.add("hidden");
 				iconDiv.classList.add("hidden");
+				icon.classList.add("hidden");
 				modalPhotoGalleryTitle.classList.add("hidden");
+				modalPhotoGallerySeparator.classList.add("hidden");
 				modalPhotoGalleryAddButton.classList.add("hidden");
 			});
 
 			backArrow.addEventListener("click", () => {
 				image.classList.remove("hidden");
 				iconDiv.classList.remove("hidden");
+				icon.classList.remove("hidden");
 				modalPhotoGalleryTitle.classList.remove("hidden");
+				modalPhotoGallerySeparator.classList.remove("hidden");
 				modalPhotoGalleryAddButton.classList.remove("hidden");
 			});
 
@@ -77,6 +88,7 @@ export const displayModalGallery = () => {
 					if (res.status === 204) {
 						image.remove();
 						iconDiv.remove();
+						window.location.reload();
 					}
 				});
 			});
